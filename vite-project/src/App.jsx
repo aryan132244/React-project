7aac6251ec348cd506069fb51ba378e37955e8a6
2222nit <nitishkamboj8070@gmail.com>
2026-02-11 12:22:16 +0530

    anm

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/navbar'
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Navbar />
+      <Navbar />
+      <Navbar />
+      <Navbar />
+    </div>
+  )
+}
