25cf893840b0c19fc3aac32ed2b6056915fd0e01
2222nit <nitishkamboj8070@gmail.com>
2026-02-25 12:40:24 +0530

    abh

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from './Components/navbar'
 import ABC from './Components/ABC'
 
+
 export default function App() {
   return (
     <div>
       <Navbar />
       <ABC/>
+    
     </div>
     
   )
--- vite-project/src/Components/ABC.jsx
+++ vite-project/src/Components/ABC.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import { FaBars } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
+
 
 export default function ABC() {
 
@@ -16,23 +19,71 @@ export default function ABC() {
     const Delete = () =>{
         b("")
     }
+
+     const menu = [
+            { href: "/", Name: "home" },
+            { href: "/", Name: "About" },
+            { href: "/", Name: "contact" },
+            { href: "/", Name: "Info" },
+            { href: "/", Name: "menu" }
+        ]
+    
+        const [open, close] = useState(false)
+    
+        const togglemeu = () => {
+            close(!open)
+        }
+        
+    
   return (
     <>
     <div>
-        <input onChange={(e) => b(e.target.value)} value={a} className=' border-black border-2 ' type="text" />
-        <br />
-        <button onClick={uppercase} className='bg-amber-200 rounded-b-lg'>uppercase</button>
-        <br />
-        <button onClick={lowercase} className='bg-amber-200 rounded-b-lg'>lowercase</button>
-        <br />
-        <button onClick={Delete} className='bg-amber-200 rounded-b-lg'>delet</button>
+        
+        <br /><br />
+        <input onChange={(e) => b(e.target.value)} value={a} className=' border-black border-2 ' type="text" placeholder='Enter any number'/>
+       <br /> <br />
+        <div className='flex gap-3'>
+            <button onClick={uppercase} className='bg-amber-200 rounded-lg  px-4 py-1'>UPPERCASE</button>
+       
+        <button onClick={lowercase} className='bg-amber-200 rounded-lg  px-4 py-1'>LOWERCASE</button>
+        
+        <button onClick={Delete} className='bg-amber-200 rounded-lg  px-4 py-1'>DELETE</button>
+        
+        </div>
+        <br /><br />
     </div>
 
-
-
-<nav>
+    <nav className='flex justify-between px-10 bg-black text-white h-10 items-center'>
+                    <div>logo</div>
     
-</nav>
+                    <ul className='  hidden md:flex gap-5'>
+    
+                        {
+                            menu.map((v, i) => (
+                                <li key={i}><a href={v.href}>{v.Name}</a></li>
+                            ))
+                        }
+                    </ul>
+                    <button className=' hidden md:block'>login</button>
+                    <div className=' block md:hidden' onClick={togglemeu}>
+                        {
+                            open ? <RxCross2/>:<FaBars/>
+                        }
+                    </div>
+                    {
+            open &&(
+              <ul className=' md:hidden bg-gray-800 text-white w-full absolute top-10 left-0 flex  flex-col gap-3 text-center'>
+              {
+                menu.map((v,i)=>(
+                    <li key={i}><a href={v.href}>{v.Name}</a></li>
+                ))
+              }
+              </ul>
+            )
+          }
+    
+                </nav>
+
     </>
   )
 }
